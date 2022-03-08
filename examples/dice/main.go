10a118e2b0e03d7dba9ff89b1c53c941c589a743
Micah Kemp <apple@micah.ws>
2022-03-07 21:15:13 -0600

    Added WriteMap

--- examples/dice/main.go
+++ examples/dice/main.go
@@ -15,23 +15,14 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/micahkemp/scad/examples/dice/die"
-	"github.com/micahkemp/scad/examples/dice/dimples"
 	"github.com/micahkemp/scad/pkg/scad"
 )
 
 func main() {
-	die := die.Die{
-		Dimple: dimples.Dimple{
-			Depth:    2,
-			Diameter: 10,
-		},
-		Width: 60,
-	}
-
-	if err := scad.Write(".", die); err != nil {
-		log.Fatalf("unable to EncodeFunction: %s", err)
+	if err := scad.WriteMap(die.DieSamples); err != nil {
+		fmt.Println("failure:", err)
 	}
 }
