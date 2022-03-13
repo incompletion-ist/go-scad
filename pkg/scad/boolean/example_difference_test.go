4545e20a96535de499d27c5d838fb6bcaac614c4
Micah Kemp <apple@micah.ws>
2022-03-12 18:35:25 -0600

    Added boolean ExampleDifference

---
+++ pkg/scad/boolean/example_difference_test.go
@@ -0,0 +1,58 @@
+// Copyright 2022 Micah Kemp
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package boolean_test
+
+import (
+	"fmt"
+
+	"github.com/micahkemp/scad/pkg/scad"
+	"github.com/micahkemp/scad/pkg/scad/boolean"
+	"github.com/micahkemp/scad/pkg/scad/primitive3d"
+	"github.com/micahkemp/scad/pkg/scad/transformation"
+	"github.com/micahkemp/scad/pkg/scad/value"
+)
+
+func ExampleDifference() {
+	var wallThickness float64 = 1
+	var outerSize float64 = 20
+
+	outerCube := primitive3d.Cube{Size: value.NewFloat(outerSize)}
+	innerCube := scad.Apply(
+		primitive3d.Cube{SizeXYZ: value.NewFloatXYZ(
+			outerSize-2*wallThickness,
+			outerSize-2*wallThickness,
+			outerSize,
+		)},
+		transformation.Translate{
+			V: value.NewFloatXYZ(wallThickness, wallThickness, wallThickness),
+		},
+	)
+
+	shell := scad.Apply(
+		outerCube,
+		boolean.Difference{
+			Children: []interface{}{innerCube},
+		},
+	)
+
+	content, _ := scad.FunctionContent(shell)
+	fmt.Println(content)
+	// Output: difference() {
+	//   cube(size=20);
+	//   translate(v=[1, 1, 1]) {
+	//     cube(size=[18, 18, 20]);
+	//   }
+	// }
+}
